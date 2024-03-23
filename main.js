72ba4452fd1f3c34042ff821135a58526f669526
chadbanghart <cbanghar@gmail.com>
2024-03-23 13:08:38 -0700

    write renderBoard()

--- main.js
+++ main.js
@@ -43,18 +43,18 @@ function render() {
   renderControls();
 }
 
-function renderBoard() {}
+function renderBoard() {
+  board.forEach(function (cellVal, cellIdx) {
+    const cellEl = document.getElementById(`idx${cellIdx}`);
+    cellEl.style.backgroundColor = PLAYER_COLORS[cellVal];
+  });
+}
 
 function renderMessage() {}
 
 function renderControls() {}
 // 4) Upon loading the app should:
-//   4.1) Initialize the state variables:
-//     4.1.1) Initialize the board array to 9 nulls to represent empty squares. The 9 elements will "map" to each square,
-//            where index 0 maps to the top-left square and index 8 maps to the bottom-right square.
-//     4.1.2) Initialize whose turn it is to 1 (player 'X'). Player 'O' will be represented by -1.
-//     4.1.3) Initialize winner to null to represent that there is no winner or tie yet. Winner will hold the player value (1 or -1)
-//            if there's a winner. Winner will hold a 'T' if there's a tie.
+
 //   4.2) Render those state variables to the page:
 //     4.2.1) Render the board:
 //       4.2.1.1) Loop over each of the 9 elements that represent the squares on the page, and for each iteration:
